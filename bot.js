da9b4b6113a794f68cabd0b615e56072c32131b1
bo3abd <37047946+bo3abd@users.noreply.github.com>
2018-08-22 05:49:12 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,6 +31,32 @@ console.log(`[M] ${client.users.size}`)
 
 
 
+  client.on('message', message => {
+    if (message.content.startsWith("رابط")) {
+
+  message.channel.createInvite({
+        thing: true,
+        maxUses: 2,
+        maxAge: 86400
+    }).then(invite =>
+      message.author.sendMessage(invite.url)
+    )
+  message.channel.send("**تم ارسال الرابط برسالة خاصة**")
+
+message.author.send(`**مدة الرابط : يـوم
+عدد استخدامات الرابط : 2**`)
+
+
+    }
+});
+  
+
+
+
+
+
+
+
 
 client.on('guildMemberAdd',async member => {
 
